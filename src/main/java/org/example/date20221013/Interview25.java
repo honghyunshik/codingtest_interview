5c94de83a3702ed564ad583065e0d2c006bfb8b7
hshong1119 <hshong1119@gmail.com>
2022-10-14 20:39:26 +0900

    2022 10 14

--- src/main/java/org/example/date20221013/Interview25.java
+++ src/main/java/org/example/date20221013/Interview25.java
@@ -4,15 +4,131 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Stack;
 
+
+//파이썬 알고리즘 인터뷰 책 25번 문제 - 원형 큐를 디자인하라(259p)
 public class Interview25 {
     public static void main(String[] args){
+        MyCircularQueue circularQueue = new MyCircularQueue(5);
+        circularQueue.enQueue(10);
+        circularQueue.enQueue(20);
+        circularQueue.enQueue(30);
+        circularQueue.enQueue(40);
+        //circularQueue.printArr();
+        //System.out.println(circularQueue.Rear());
+        //System.out.println(circularQueue.isFull());
+        System.out.println(circularQueue.deQueue());
+        System.out.println(circularQueue.deQueue());
+        //circularQueue.printArr();
+        System.out.println(circularQueue.enQueue(50));
+        System.out.println(circularQueue.enQueue(60));
+        circularQueue.printArr();
+        //System.out.println(circularQueue.Rear());
+        System.out.println(circularQueue.Front());
 
-        Deque<Integer> stack = new LinkedList<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.pollLast();
-        System.out.println(stack);
+
     }
 }
+
+class MyCircularQueue{
+
+    Integer[] arr;
+    int front;      //큐의 가장 앞 index
+    int back;       //큐의 가장 끝 index
+
+    MyCircularQueue(int size){
+        arr = new Integer[size];
+        front = 0;
+        back = 0;
+    }
+
+    //원형 큐에 요소를 삽입, 성공시 true 반환
+    public boolean enQueue(int num){
+
+        if(isFull()){
+            return false;
+        }
+
+        if(!isEmpty()){
+            if(back==arr.length-1){
+                back = 0;
+            }else{
+                back++;
+            }
+        }
+
+        System.out.println("front : " + front + " back : " + back);
+
+        arr[back] = num;
+
+
+        return true;
+    }
+
+    //원형 큐의 요소를 삭제, 성공시 true 반환
+    public boolean deQueue(){
+
+        if(isEmpty()){
+            return false;
+        }
+
+        arr[front] = null;
+
+        if(front==arr.length-1){
+            front = 0;
+        }else{
+            front++;
+        }
+
+
+        System.out.println("front : " + front + " back : " + back);
+
+
+        return true;
+    }
+
+    //큐에서 마지막 요소를 가져온다. 큐가 비어있다면 -1을 반환
+    public int Rear(){
+
+        if(arr[back]!=null){
+            return arr[back];
+        }
+        return -1;
+    }
+
+    //큐가 가득 차 있는지 확인
+    public boolean isFull(){
+        for(Integer num:arr){
+            if(num==null){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //큐가 비어 있는지 확인
+    public boolean isEmpty(){
+        if(front==back&&arr[front]==null){
+            return true;
+        }
+        return false;
+    }
+
+    //큐에서 처음 요소를 가져온다. 큐가 비어있다면 -1을 반환
+    public int Front(){
+        if(arr[front]!=null){
+            return arr[front];
+        }
+        return -1;
+    }
+
+    public void printArr(){
+        for(Integer num:arr){
+            System.out.print(num + " ");
+        }
+        System.out.println("");
+    }
+
+
+}
+
+
---
+++ src/main/java/org/example/date20221014/Interview26.java
@@ -0,0 +1,61 @@
+package org.example.date20221014;
+
+
+//파이썬 알고리즘 인터뷰 책 26번 문제 - 원형 데크 디자인(259p)
+public class Interview26 {
+    public static void main(String[] args){
+
+    }
+}
+
+class MyCircularDeque{
+
+    MyCircularDeque(int size){
+
+    }
+
+    //데크 처음에 아이템을 추가하고 성공할 경우 true를 리턴한다
+    public boolean insertFront(){
+        return true;
+    }
+
+    //데크 마지막에 아이템을 추가하고 성공할 경우 true를 리턴한다
+    public boolean insertLast(){
+        return true;
+    }
+
+    //데크 처음에 아이템을 삭제하고 성공할 경우 true를 리턴한다
+    public boolean deleteFront(){
+        return true;
+    }
+
+    //데크 마지막 아이템을 삭제하고 성공할 경우 true를 리턴한다
+    public boolean deleteLast(){
+        return true;
+    }
+
+    //데크의 첫 번째 아이템을 가져온다. 데크가 비어 있다면 -1을 리턴한다
+    public int getFront(){
+        return -1;
+    }
+
+    //데크의 마지막 아이템을 가져온다. 데크가 비어 있다면 -1을 리턴한다
+    public int getRear(){
+        return -1;
+    }
+
+    //데크가 비어 있는지 여부를 판별한다
+    public boolean isEmpty(){
+        return true;
+    }
+
+    //데크가 가득 차 있는지 여부를 판별한다
+    public boolean isFull(){
+        return true;
+    }
+
+
+
+
+}
+
