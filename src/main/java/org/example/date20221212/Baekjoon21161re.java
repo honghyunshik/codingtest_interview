56435f688128d4258348f66561da02ebbaeff3c0
hshong1119 <hshong1119@gmail.com>
2022-12-21 22:09:45 +0900

    2022 12 21

--- src/main/java/org/example/date20221212/Baekjoon21161re.java
+++ src/main/java/org/example/date20221212/Baekjoon21161re.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
+class Pointer3{
+    int l,r,dis;
+    Pointer3(int l,int r,int dis){
+        this.l = l;
+        this.r = r;
+        this.dis = dis;
+    }
+}
 public class Baekjoon21161re {
     static int N;
     static int M;
@@ -18,6 +27,8 @@ public class Baekjoon21161re {
     static Queue<int[]> forChange = new LinkedList<>();
     static int forNextL,forNextR,forNextState = 0;
     static int answer = 0;
+    static ArrayList<ArrayList<Pointer3>> forDelete;
+    static int nextL, nextR;
 
 
 
@@ -28,6 +39,8 @@ public class Baekjoon21161re {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         MARBLE = new int[N][N];
+
+        forDelete = new ArrayList<>();
 
         for(int i=0;i<N;i++){
             String[] input = br.readLine().split(" ");
@@ -42,12 +55,10 @@ public class Baekjoon21161re {
             st = new StringTokenizer(br.readLine()," ");
             direction = Integer.parseInt(st.nextToken());
             distance = Integer.parseInt(st.nextToken());
-            magic();
-            bomb();
-            change(N/2,N/2-1);
-            for(int j=0;j<CLONE.length;j++){
-                MARBLE[j] = CLONE[j];
-            }
+            //magic();
+            //bomb();
+            //change(N/2,N/2-1);
+
             for(int j=0;j<MARBLE.length;j++){
                 System.out.println(Arrays.toString(MARBLE[j]));
             }
@@ -57,6 +68,53 @@ public class Baekjoon21161re {
         System.out.println(answer);
 
 
+    }
+
+    static void findState(Pointer3 curr){
+        int nextL = curr.l;
+        int nextR = curr.r;
+        //왼쪽 대각선 위
+        if(curr.l==curr.r+1&&curr.l<=N/2){
+            nextL++;
+            state = 1;
+        }
+        //왼쪽 대각선 아래
+        else if(curr.l-N/2==N/2-curr.r&&curr.r<N/2){
+            nextR++;
+            state = 6;
+        }
+        //오른쪽 대각선 위
+        else if(curr.l-N/2==N/2-curr.r&&curr.r>N/2){
+            nextR--;
+            state = 3;
+        }
+        //오른쪽 대각선 아래
+        else if(curr.l==curr.r&&curr.l>N/2){
+            nextL--;
+            state = 8;
+        }
+        //그냥 위
+        else if(state==3||state==2){
+            nextR--;
+            state = 2;
+        }
+
+        //그냥 왼쪽
+        else if(state==1||state==4){
+            nextL++;
+            state = 4;
+        }
+        //그냥 오른쪽
+        else if(state==8||state==5){
+            nextL--;
+            state = 5;
+        }
+
+        //그냥 아래
+        else if(state==6||state==7){
+            nextR++;
+            state = 7;
+        }
     }
 
     static void change(int startL, int startR){
@@ -76,48 +134,8 @@ public class Baekjoon21161re {
             int nextR = startR;
 
             for(int k=0;k<2;k++){
-                //왼쪽 대각선 위
-                if(startL==startR+1&&startL<=N/2){
-                    nextL++;
-                    state = 1;
-                }
-                //왼쪽 대각선 아래
-                else if(startL-N/2==N/2-startR&&startR<N/2){
-                    nextR++;
-                    state = 6;
-                }
-                //오른쪽 대각선 위
-                else if(startL-N/2==N/2-startR&&startR>N/2){
-                    nextR--;
-                    state = 3;
-                }
-                //오른쪽 대각선 아래
-                else if(startL==startR&&startL>N/2){
-                    nextL--;
-                    state = 8;
-                }
-                //그냥 위
-                else if(state==3||state==2){
-                    nextR--;
-                    state = 2;
-                }
 
-                //그냥 왼쪽
-                else if(state==1||state==4){
-                    nextL++;
-                    state = 4;
-                }
-                //그냥 오른쪽
-                else if(state==8||state==5){
-                    nextL--;
-                    state = 5;
-                }
-
-                //그냥 아래
-                else if(state==6||state==7){
-                    nextR++;
-                    state = 7;
-                }
+                findState(new Pointer3(startL,startR,0));
                 if(k==0){
                     CLONE[startL][startR] = count;
                     CLONE[nextL][nextR] = num;
@@ -139,40 +157,7 @@ public class Baekjoon21161re {
     }
 
 
-    static void magic(){
 
-        int centerL = N/2;
-        int centerR = N/2;
-
-        //마법이 발동한 후 구슬이 사라진다
-        for(int i=0;i<distance;i++){
-            centerL += D[direction-1][0];
-            centerR += D[direction-1][1];
-            MARBLE[centerL][centerR] = 0;
-        }
-
-        for(int k=distance;k>=1;k--){
-
-            int startL = N/2 + D[direction-1][0]*k;
-            int startR = N/2 + D[direction-1][1]*k;
-
-
-            if(direction==1){
-                startR--;
-            }else if(direction==2){
-                startR++;
-            }else if(direction==3){
-                startL++;
-            }else if(direction==4){
-                startL--;
-            }
-
-            //구슬이 사라진 후 구슬이 이동한다
-            dfs(startL,startR,0);
-        }
-
-
-    }
 
     static void bomb(){
 
@@ -209,64 +194,23 @@ public class Baekjoon21161re {
 
         delete.clear();
         forChange.clear();
-        Stack<Pointer> stack = new Stack<>();
+        Stack<Pointer3> stack = new Stack<>();
         ;
-        stack.add(new Pointer(N/2,N/2-1,1));
+        stack.add(new Pointer3(N/2,N/2-1,1));
+        ArrayList<Pointer3> temp = new ArrayList<>();
 
         while(!stack.isEmpty()){
 
-            Pointer curr = stack.pop();
+            Pointer3 curr = stack.pop();
             delete.add(new int[]{curr.l,curr.r,state});
             int nextL = curr.l;
             int nextR = curr.r;
-            //왼쪽 대각선 위
-            if(curr.l==curr.r+1&&curr.l<=N/2){
-                nextL++;
-                state = 1;
-            }
-            //왼쪽 대각선 아래
-            else if(curr.l-N/2==N/2-curr.r&&curr.r<N/2){
-                nextR++;
-                state = 6;
-            }
-            //오른쪽 대각선 위
-            else if(curr.l-N/2==N/2-curr.r&&curr.r>N/2){
-                nextR--;
-                state = 3;
-            }
-            //오른쪽 대각선 아래
-            else if(curr.l==curr.r&&curr.l>N/2){
-                nextL--;
-                state = 8;
-            }
-            //그냥 위
-            else if(state==3||state==2){
-                nextR--;
-                state = 2;
-            }
-
-            //그냥 왼쪽
-            else if(state==1||state==4){
-                nextL++;
-                state = 4;
-            }
-            //그냥 오른쪽
-            else if(state==8||state==5){
-                nextL--;
-                state = 5;
-            }
-
-            //그냥 아래
-            else if(state==6||state==7){
-                nextR++;
-                state = 7;
-            }
+            findState(curr);
 
             forNextL = nextL;
             forNextR = nextR;
 
             if(MARBLE[nextL][nextR]!=MARBLE[curr.l][curr.r]){
-                forChange.add(new int[]{MARBLE[curr.l][curr.r],Math.min(curr.dis,3)});
                 if(curr.dis>=4){
                     System.out.println(curr.dis);
                     return;
@@ -275,7 +219,7 @@ public class Baekjoon21161re {
                 delete.clear();
             }
             if(MARBLE[nextL][nextR]==0) return;
-            stack.push(new Pointer(nextL,nextR,curr.dis+1));
+            stack.push(new Pointer3(nextL,nextR,curr.dis+1));
 
 
 
@@ -293,8 +237,8 @@ public class Baekjoon21161re {
 
         }
 
-        Stack<Pointer> stack = new Stack<>();
-        stack.add(new Pointer(startL,startR));
+        Stack<Pointer3> stack = new Stack<>();
+        stack.add(new Pointer3(startL,startR,0));
         if(forState==0){
             if(direction==1){
                 state = 2;
@@ -311,53 +255,8 @@ public class Baekjoon21161re {
 
         while(!stack.isEmpty()){
 
-            Pointer curr = stack.pop();
-            int nextL = curr.l;
-            int nextR = curr.r;
-            //왼쪽 대각선 위
-            if(curr.l==curr.r+1&&curr.l<=N/2){
-                nextL++;
-                state = 1;
-            }
-            //왼쪽 대각선 아래
-            else if(curr.l-N/2==N/2-curr.r&&curr.r<N/2){
-                nextR++;
-                state = 6;
-            }
-            //오른쪽 대각선 위
-            else if(curr.l-N/2==N/2-curr.r&&curr.r>N/2){
-                nextR--;
-                state = 3;
-            }
-            //오른쪽 대각선 아래
-            else if(curr.l==curr.r&&curr.l>N/2){
-                nextL--;
-                state = 8;
-            }
-            //그냥 위
-            else if(state==3||state==2){
-                nextR--;
-                state = 2;
-            }
-
-            //그냥 왼쪽
-            else if(state==1||state==4){
-                nextL++;
-                state = 4;
-            }
-            //그냥 오른쪽
-            else if(state==8||state==5){
-                nextL--;
-                state = 5;
-            }
-
-            //그냥 아래
-            else if(state==6||state==7){
-                nextR++;
-                state = 7;
-            }
-
-
+            Pointer3 curr = stack.pop();
+            findState(curr);
 
             for(int i=0;i<4;i++){
 
@@ -381,7 +280,7 @@ public class Baekjoon21161re {
                 MARBLE[zeroL][zeroR] = temp;
                 if(nextL<0||nextL>N-1||nextR<0||nextR>N-1||MARBLE[nextL][nextR]==0) return;
 
-                stack.push(new Pointer(nextL,nextR));
+                stack.push(new Pointer3(nextL,nextR,0));
 
 
             }
