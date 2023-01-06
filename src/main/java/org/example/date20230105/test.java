e556af4f941e5c6a0044a3f4152b4ade6d9465ca
hshong1119 <hshong1119@gmail.com>
2023-01-06 17:35:30 +0900

    2023 01 06

---
+++ src/main/java/org/example/date20230105/test.java
@@ -0,0 +1,11 @@
+package org.example.date20230105;
+
+public class test {
+
+    public static void main(String[] args){
+        Gift[] a = new Gift[8];
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]== null);
+        }
+    }
+}
