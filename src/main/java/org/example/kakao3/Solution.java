b8b612bec4b74d8ce2d8f1568f7d58f214ecc472
honghyunshik <hshong0907@gmail.com>
2023-01-18 23:39:42 +0900

    2023 01 18

---
+++ src/main/java/org/example/kakao3/Solution.java
@@ -0,0 +1,20 @@
+package org.example.kakao3;
+
+public class Solution {
+
+    public static void main(String[] args){
+
+    }
+
+
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+
+        //n,m은 각각 세로 좌표, 가로 좌표
+        //x,y는 출발 위치를 뜻하는 좌표
+        //r,c 는 탈출 지점을 나타내는 좌표
+        //k는 탈출해야 하는 이동거리
+
+        String answer = "";
+        return answer;
+    }
+}
