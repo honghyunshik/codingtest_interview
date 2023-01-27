c747e041e9e3ec4f39d35f3ad6313e50d86f764e
honghyunshik <hshong0907@gmail.com>
2023-01-27 23:00:26 +0900

    2023 01 27

--- src/main/java/org/example/baekjoon11047/Main.java
+++ src/main/java/org/example/baekjoon11047/Main.java
@@ -1,8 +1,39 @@
 package org.example.baekjoon11047;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        Integer[] coin = new Integer[n];
+        for(int i=0;i<n;i++){
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coin, Collections.reverseOrder());
+
+        int count = 0;
+        for(int num:coin){
+
+            if(target>=num){
+                int temp = target/num;
+                target -= temp*num;
+                count += temp;
+            }
+            if(target==0) break;
+        }
+        System.out.println(count);
 
 
     }
