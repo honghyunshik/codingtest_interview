bc0b505681940adb0a036f6e4d4f29ee27cad666
honghyunshik <hshong0907@gmail.com>
2023-01-28 22:31:52 +0900

    2023 01 27

---
+++ src/main/java/org/example/baekjoon11399/Main.java
@@ -0,0 +1,34 @@
+package org.example.baekjoon11399;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        Integer[] arr = new Integer[n];
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int sum = 0;
+        int now = 0;
+        for(int i=0;i<arr.length;i++){
+            now += arr[i];
+            sum+= now;
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
