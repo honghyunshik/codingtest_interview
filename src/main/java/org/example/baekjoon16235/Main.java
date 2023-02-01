9f17e794c42102182894777e35946ceade1977e0
honghyunshik <hshong0907@gmail.com>
2023-02-01 23:19:28 +0900

    2023 01 31

--- src/main/java/org/example/baekjoon16235/Main.java
+++ src/main/java/org/example/baekjoon16235/Main.java
@@ -1,8 +1,47 @@
 package org.example.baekjoon16235;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=0;j<n;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] tree = new int[m][3];
+        for(int i=0;i<m;i++){
+             st = new StringTokenizer(br.readLine()," ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            tree[i] = new int[]{x,y,z};
+        }
+
 
     }
+}
+
+class Solution{
+
+    //
+
+
+
 }
