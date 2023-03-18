48027f503cff6f8b68e4dfad3aa4240a022f9c39
honghyunshik <hshong0907@gmail.com>
2023-03-19 00:39:53 +0900

    2023 03 04

---
+++ src/main/java/org/example/programmers2/asdf.java
@@ -0,0 +1,15 @@
+package org.example.programmers2;
+
+import java.util.PriorityQueue;
+
+public class asdf {
+
+    public int dijkstra(){
+        int n = 100;
+        boolean[] visited = new boolean[100];
+
+
+        return 90;
+    }
+}
+
