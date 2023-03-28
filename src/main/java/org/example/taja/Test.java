f99d8d3952e5b420a77cdf27f22360f226189ee6
honghyunshik <hshong0907@gmail.com>
2023-03-28 20:43:14 +0900

    2023 03 04

--- src/main/java/org/example/taja/Test.java
+++ src/main/java/org/example/taja/Test.java
@@ -4,14 +4,29 @@ public class Test {
 
     public static void main(String[] args){
 
+        /*
         String a = "123";
         change(a);
         System.out.println(a);
+        */
+        Student<Integer,Integer> student = new Student<>(100000,15);
+        System.out.println(student.age);
+        System.out.println(student.money);
 
     }
 
     public static void change(String s){
         s = s.concat("123");
 
+    }
+}
+
+class Student<S,T>{
+
+    S money;
+    T age;
+    Student(S money, T age){
+        this.money = money;
+        this.age = age;
     }
 }
