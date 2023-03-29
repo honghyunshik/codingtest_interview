1bb7eccda9dcf442e33d7b171b7def8260d31552
honghyunshik <hshong0907@gmail.com>
2023-03-29 22:37:44 +0900

    클래스 변수 초기화 순서

--- src/main/java/org/example/taja/Practice.java
+++ src/main/java/org/example/taja/Practice.java
@@ -2,14 +2,27 @@ package org.example.taja;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Practice {
 
     public static void main(String[] args){
 
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4));
-        Stream<?> stream = list.stream();
-        stream.forEach(System.out::println);
+        //ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,31,4,89));
+        //List<Integer> list2 = list.stream().map(num->num+100).filter(num->num>2).sorted((a, b)->b-a).collect(Collectors.toList());
+        //int a = (int) stream.count();
+        //System.out.println(a);
+        Hi hi = new Hi(20);
+        System.out.println(hi.a);
     }
+}
+
+class Hi{
+    Hi(int a){
+        this.a = a;
+    }
+
+    int a = 10;
 }
