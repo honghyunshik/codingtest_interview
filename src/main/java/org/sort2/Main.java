cdeb08ee2afb95751f363afa66ce154e43ff9a6c
honghyunshik <hshong0907@gmail.com>
2023-05-13 17:45:33 +0900

    선택정렬

--- src/main/java/org/sort2/Main.java
+++ src/main/java/org/sort2/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
     public static void main(String[] args){
 
-        BubbleSort bubbleSort = new BubbleSort();
         int[] arr = {1,24,274,1111,28,0,8,33};
-        bubbleSort.bubble_sort(arr);
+
+        //BubbleSort bubbleSort = new BubbleSort();
+        //bubbleSort.bubble_sort(arr);
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.selection_sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
---
+++ src/main/java/org/sort2/SelectionSort.java
@@ -0,0 +1,27 @@
+package org.sort2;
+
+public class SelectionSort {
+
+    public void selection_sort(int[] arr){
+
+        for(int i=0;i<arr.length-1;i++){
+
+            int min_idx = i;
+
+            for(int j=i+1;j<arr.length;j++){
+
+                if(arr[min_idx]>arr[j]) min_idx = j;
+            }
+
+            swap(arr,i,min_idx);
+        }
+
+    }
+
+    private void swap(int[] arr, int i, int j){
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
